55fc71e93ee4c9660a46f3e50c4ebf314f03b450
Anna S. Atalay <anna.atalay@minna.tech>
2025-01-20 18:22:20 +0100

    Test score algorithm and updated test data for Reading assignment (#4)
    Implement test score algorithm and update reading test

--- src/app/test/reading/__test__/readingData.test.tsx
+++ src/app/test/reading/__test__/readingData.test.tsx
@@ -1,24 +1,25 @@
-import { Passage, readingPassages } from '../readingData';
+import { ReadingTest } from "modules/app/lib/models/test";
+import { data } from "../../data";
 
-const checkForDuplicateUUIDs = (passages: Passage[]) => {
+const checkForDuplicateUUIDs = (passages: ReadingTest[]) => {
   const uuidSet = new Set<string>();
   const duplicateUUIDs: string[] = [];
 
-  passages.forEach(passage => {
+  passages.forEach((passage) => {
     if (uuidSet.has(passage.id)) {
       duplicateUUIDs.push(passage.id);
     } else {
       uuidSet.add(passage.id);
     }
 
-    passage.questions.forEach(question => {
+    passage.questions.forEach((question) => {
       if (uuidSet.has(question.id)) {
         duplicateUUIDs.push(question.id);
       } else {
         uuidSet.add(question.id);
       }
 
-      question.options.forEach(option => {
+      question.options.forEach((option) => {
         if (uuidSet.has(option.id)) {
           duplicateUUIDs.push(option.id);
         } else {
@@ -31,7 +32,11 @@ const checkForDuplicateUUIDs = (passages: Passage[]) => {
   return duplicateUUIDs;
 };
 
-test('No duplicated UUIDs in readingPassages', () => {
-  const duplicateUUIDs = checkForDuplicateUUIDs(readingPassages);
+test("No duplicated UUIDs in readingPassages", () => {
+  const duplicateUUIDs = checkForDuplicateUUIDs([
+    ...data.reading.A1_A2,
+    ...data.reading.B1_B2,
+    ...data.reading.C1_C2,
+  ]);
   expect(duplicateUUIDs).toHaveLength(0);
 });
