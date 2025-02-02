9859a5c53c2238b50e1ed3378834c4664fc481fc
Anna S. A <anna.atalay@minna.tech>
2025-02-02 01:47:25 +0100

    Fetch user data on the results page

--- src/app/test/result/page.tsx
+++ src/app/test/result/page.tsx
@@ -2,24 +2,60 @@
 
 import Link from "next/link";
 import { Button, ButtonColor } from "modules/app/_components/Button";
-import { useUserLevel } from "../UserLevelProvider";
+import { TestResults } from "../_components/TestResults";
+import { useEffect, useState } from "react";
+import { ImageBroken } from "@phosphor-icons/react";
+
+interface User {
+  name: string;
+  email: string;
+}
 
 export default function Result() {
-  const { state } = useUserLevel();
-  console.log(state);
+  const [user, setUser] = useState<User | null>(null);
 
-  const readingLevel = state.reading;
-  const listeningLevel = state.listening;
-  const grammarLevel = state.grammar;
+  useEffect(() => {
+    async function fetchUser() {
+      const userResponse = await fetch("/api/user");
+      if (userResponse.ok) {
+        const userData = await userResponse.json();
+        setUser(userData);
+      } else {
+        console.error("Error fetching user", userResponse);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  if (!user) {
+    return (
+      <div className="bg-gradient-to-r from-gray-50 to-gray-100 text-gray-900 min-h-screen text-center py-20 px-8 md:px-20">
+        <div className="flex justify-center items-center mb-8">
+          <ImageBroken size={240} />
+        </div>
+        <p className="text-lg">
+          Oh no, something went wrong when fetching results. Send us an email at{" "}
+          <Link
+            href="mailto:hello@code2career.eu"
+            className="underline hover:text-teal-800 hover:cursor-pointer"
+          >
+            hello@code2career.eu
+          </Link>{" "}
+          and we&apos;ll try to help.
+        </p>
+        <div className="flex justify-center items-center my-8">
+        <Button color={ButtonColor.Primary} ><Link href="/">To the home page</Link></Button>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Congratulations, you have completed the test</h1>
+        <h1>Congratulations {user.name}, you have completed the test!</h1>
         <h2>Your result</h2>
-        <p>Reading: {readingLevel}</p>
-        <p>Listening: {listeningLevel}</p>
-        <p>Grammar: {grammarLevel}</p>
+        <TestResults />
         <Button color={ButtonColor.Primary}>
           <Link href="/">Return to the main page</Link>
         </Button>
