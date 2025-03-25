bf5d8de73a91dd4ab5bcf5d2b91b0eda0d6b9462
code2career <info@annaatalay.com>
2025-03-25 20:28:56 +0100

    Update formatting and git ignore secret file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/speaking-club', request.url))
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/speaking-club", request.url));
   }
-  return NextResponse.next()
+  return NextResponse.next();
 }
