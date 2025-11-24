b73ee5425e03f06f90c7100b116a44a73e311c7a
annasvst <annaatalay@hotmail.com>
2025-11-24 10:36:40 +0100

    Remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
  
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/speaking-club', request.url))
-  }
+export function middleware() {
+  // if (request.nextUrl.pathname === '/') {
+  //   return NextResponse.redirect(new URL('/speaking-club', request.url))
+  // }
   return NextResponse.next()
 }
