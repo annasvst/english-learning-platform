ce62d91314c7a23019a420d18a05b560da75b16c
annasvst <annaatalay@hotmail.com>
2025-11-24 10:38:56 +0100

    refactor: Simplify Error component by removing unused error prop

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,7 @@
+'use client';
 import Link from 'next/link';
 
-export default function Error({
-  error,
-}: {
-  error: Error & { digest?: string };
-}) {
+export default function Error() {
   return (
     <div className="min-h-screen bg-rose-50 flex items-center justify-center p-4">
       <div className="max-w-md w-full text-center">
